5ef7b34e25a4d65097f3197959131f564de18241
Vis Virial <visvirial@gmail.com>
2025-01-19 12:50:12 +0900

    First commit.

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,114 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import bcrypt from 'bcryptjs';
+
+export const authorize = async (env: Env, body: any): Promise<boolean> => {
+	const data = await env.WALLETS.get(body.email);
+	if(!data) {
+		return false;
+	}
+	const json = JSON.parse(data);
+	return await bcrypt.compare(body.password, json.passwordHash);
+};
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const method = request.method;
+		const body = await (async () => {
+			if(method === 'GET') {
+				const json = {};
+				for(const [key, value] of url.searchParams.entries()) {
+					json[key] = value;
+				}
+				return json;
+			}
+			if(method === 'POST') {
+				try {
+					const body = await new Response(request.body).json();
+					return body;
+				} catch (e) {
+					return new Response('Invalid JSON', { status: 400 });
+				}
+			}
+		})();
+		//console.log('Body:', body);
+		if(body) {
+			// Check if the email parameter is set.
+			if(!body.email) {
+				return new Response('Parameter "email" not set.', { status: 400 });
+			}
+			// Check if the password parameter is set.
+			if(!body.password) {
+				return new Response('Parameter "password" not set.', { status: 400 });
+			}
+		}
+		if(url.pathname === '/register' && method === 'POST') {
+			// POST /register.
+			// If the user already exists, return 409.
+			const _data = await env.WALLETS.get(body.email);
+			if(_data) {
+				return new Response('User already exists', { status: 409 });
+			}
+			// Create a new user.
+			const passwordhash = await bcrypt.hash(body.password, 10);
+			//console.log('Password hash:', passwordHash);
+			const data = {
+				passwordHash,
+				wallets: '',
+			};
+			await env.WALLETS.put(body.email, JSON.stringify(data));
+			return new Response('User created', { status: 201 });
+		} else if(url.pathname === '/changepw' && method === 'POST') {
+			// POST /changepw.
+			// Check if the newPassword parameter is set.
+			if(!body.newPassword) {
+				return new Response('Parameter "newPassword" not set.', { status: 400 });
+			}
+			if(!await authorize(env, body)) {
+				return new Response('Unauthorized', { status: 401 });
+			}
+			// Change the password.
+			const passwordHash = await bcrypt.hash(body.newPassword, 10);
+			const data = await env.WALLETS.get(body.email);
+			const json = JSON.parse(data);
+			json.passwordHash = passwordHash;
+			await env.WALLETS.put(body.email, JSON.stringify(json));
+			return new Response('Password changed');
+		} else if(url.pathname === '/get' && method === 'GET') {
+			// GET /get.
+			if(!await authorize(env, body)) {
+				return new Response('Unauthorized', { status: 401 });
+			}
+			const data = await env.WALLETS.get(body.email);
+			const json = JSON.parse(data);
+			return new Response(json.wallets);
+		} else if(url.pathname === '/set' && method === 'POST') {
+			// POST /set.
+			// Check if the wallets parameter is set.
+			if(!body.wallets) {
+				return new Response('Parameter "wallets" not set.', { status: 400 });
+			}
+			if(!await authorize(env, body)) {
+				return new Response('Unauthorized', { status: 401 });
+			}
+			// Set the wallets.
+			const data = await env.WALLETS.get(body.email);
+			const json = JSON.parse(data);
+			json.wallets = body.wallets;
+			await env.WALLETS.put(body.email, JSON.stringify(json));
+		} else if(url.pathname === '/get' && method === 'GET') {
+			// GET /get.
+			if(!await authorize(env, body)) {
+				return new Response('Unauthorized', { status: 401 });
+			}
+			const data = await env.WALLETS.get(body.email);
+			const json = JSON.parse(data);
+			return new Response(json.wallets);
+		} else {
+			return new Response('Not found', { status: 404 });
+		}
 	},
 } satisfies ExportedHandler<Env>;
+
---
+++ src/script/query.ts
@@ -0,0 +1,90 @@
+
+import fs from 'fs/promises';
+
+const URL_PREFIX = 'https://wallets.defi-portfolio.visvirial.com';
+
+export const main = async () => {
+	if(process.argv.length < 3) {
+		console.log('Usage: node query.js <register|changepw|get|set> [<args...>]');
+		return;
+	}
+	const method = process.argv[2];
+	const args = process.argv.slice(3);
+	if(method === 'register') {
+		if(args.length < 2) {
+			console.log('Usage: node query.js register <email> <password>');
+			return;
+		}
+		const email = args[0];
+		const password = args[1];
+		const response = await fetch(`${URL_PREFIX}/register`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+		});
+		console.log(await response.text());
+	} else if(method === 'changepw') {
+		if(args.length < 3) {
+			console.log('Usage: node query.js changepw <email> <oldPassword> <newPassword>');
+			return;
+		}
+		const email = args[0];
+		const password = args[1];
+		const newPassword = args[2];
+		const response = await fetch(`${URL_PREFIX}/changepw`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email,
+				password,
+				newPassword,
+			}),
+		});
+		console.log(await response.text());
+	} else if(method === 'get') {
+		if(args.length < 2) {
+			console.log('Usage: node query.js get <email> <password>');
+			return;
+		}
+		const email = args[0];
+		const password = args[1];
+		const params = new URLSearchParams();
+		params.append('email', email);
+		params.append('password', password);
+		const response = await fetch(`${URL_PREFIX}/get?${params.toString()}`);
+		console.log(await response.text());
+	} else if(method === 'set') {
+		if(args.length < 3) {
+			console.log('Usage: node query.js set <email> <password> <wallet.yaml>');
+			return;
+		}
+		const email = args[0];
+		const password = args[1];
+		const walletFile = args[2];
+		const wallets = await fs.readFile(walletFile, 'utf8');
+		const response = await fetch(`${URL_PREFIX}/set`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email,
+				password,
+				wallets,
+			}),
+		});
+		console.log(await response.text());
+	} else {
+		console.log('Invalid method:', method);
+	}
+};
+
+main();
+
